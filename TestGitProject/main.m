ee276518b91c64833aa7c482bdff4917bb0b32f6
Fiseha <fiseha@Fisehas-MacBook-Air.local>
2015-05-11 16:12:42 -0400

    Initial Commit

---
+++ TestGitProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGitProject
+//
+//  Created by Fiseha on 5/11/15.
+//  Copyright (c) 2015 Fiseha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
